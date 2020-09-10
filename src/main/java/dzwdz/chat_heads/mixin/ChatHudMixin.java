13092d33222e05da81b90b847bbd26053b6df206
Dźwiedź <kg67199@gmail.com>
2020-09-10 16:41:48 +0200

    Fix a few cosmetic bugs

--- src/main/java/dzwdz/chat_heads/mixin/ChatHudMixin.java
+++ src/main/java/dzwdz/chat_heads/mixin/ChatHudMixin.java
@@ -8,20 +8,19 @@ import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.ChatHudLine;
 import net.minecraft.client.network.PlayerListEntry;
-import net.minecraft.client.render.entity.PlayerModelPart;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ChatHud.class)
-public class ChatHudMixin {
+public abstract class ChatHudMixin {
     @Shadow @Final private MinecraftClient client;
 
     @Inject(
@@ -70,5 +69,16 @@ public class ChatHudMixin {
     )
     public int correctClickPosition(int x) {
         return x - EntryPoint.CHAT_OFFSET;
+    }
+
+    @Redirect(
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/hud/ChatHud;getWidth()I"
+            ),
+            method = "addMessage(Lnet/minecraft/text/StringRenderable;IIZ)V"
+    )
+    public int fixTextOverflow(ChatHud chatHud) {
+        return ChatHud.getWidth(client.options.chatWidth) - EntryPoint.CHAT_OFFSET;
     }
 }
