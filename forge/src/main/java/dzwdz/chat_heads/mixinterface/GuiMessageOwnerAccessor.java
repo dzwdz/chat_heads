4fce2d9b13e7dab9b79fd4e16c527980b26fb17a
dzwdz <kg67199@gmail.com>
2020-10-29 10:06:42 +0100

    yeah

--- forge/src/main/java/dzwdz/chat_heads/mixinterface/GuiMessageOwnerAccessor.java
+++
@@ -1,7 +0,0 @@
-package dzwdz.chat_heads.mixinterface;
-
-import net.minecraft.client.network.play.NetworkPlayerInfo;
-
-public interface GuiMessageOwnerAccessor {
-    NetworkPlayerInfo chatheads$getOwner();
-}
