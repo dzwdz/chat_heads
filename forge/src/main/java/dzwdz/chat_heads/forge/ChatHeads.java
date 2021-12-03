ac46cf8cc13a705e07dfcfb5f7217973d1d303a8
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-12-03 07:29:28 +0100

    update to 1.18 forge

--- forge/src/main/java/dzwdz/chat_heads/forge/ChatHeads.java
+++ forge/src/main/java/dzwdz/chat_heads/forge/ChatHeads.java
@@ -5,14 +5,14 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod("chat_heads")
 public class ChatHeads {
 	public ChatHeads() {
 		// Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
 		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
-				() -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+				() -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 
 		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ChatHeadsClient::init);
 	}
--- forge/src/main/java/dzwdz/chat_heads/forge/config/ClothConfigImpl.java
+++ forge/src/main/java/dzwdz/chat_heads/forge/config/ClothConfigImpl.java
@@ -6,17 +6,17 @@ import dzwdz.chat_heads.config.ChatHeadsConfigData;
 import dzwdz.chat_heads.config.MissingClothConfigScreen;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fmlclient.ConfigGuiHandler.ConfigGuiFactory;
 
 public class ClothConfigImpl {
 	private static boolean isInstalled() {
-		return ModList.get().getModFileById("cloth_config") != null;
+		return ModList.get().isLoaded("cloth_config");
 	}
 
 	public static void registerConfigGui() {
-		ModLoadingContext.get().registerExtensionPoint(ConfigGuiFactory.class, () -> new ConfigGuiFactory(
+		ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory(
 				(client, parent) -> {
 					if (isInstalled()) {
 						return AutoConfig.getConfigScreen(ChatHeadsConfigData.class, parent).get();
