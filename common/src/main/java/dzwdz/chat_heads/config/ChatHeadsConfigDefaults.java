2ea9ac66a662f75e6e145f2fe4f5be2eaf85dfea
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-09-19 20:45:42 +0200

    handle system messages by default and add clarifying description

--- common/src/main/java/dzwdz/chat_heads/config/ChatHeadsConfigDefaults.java
+++ common/src/main/java/dzwdz/chat_heads/config/ChatHeadsConfigDefaults.java
@@ -4,7 +4,7 @@ public class ChatHeadsConfigDefaults implements ChatHeadsConfig {
 	public static final boolean OFFSET_NON_PLAYER_TEXT = true;
 	public static final SenderDetection SENDER_DETECTION = SenderDetection.UUID_AND_HEURISTIC;
 	public static final boolean SMART_HEURISTICS = true;
-	public static final boolean HANDLE_SYSTEM_MESSAGES = false;
+	public static final boolean HANDLE_SYSTEM_MESSAGES = true;
 
 	@Override
 	public boolean offsetNonPlayerText() {
