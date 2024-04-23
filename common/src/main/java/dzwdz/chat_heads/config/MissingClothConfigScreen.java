0c994b6e0edfd94abba7d39e9a73d2bdc97dc7a6
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2024-04-24 00:43:39 +0200

    add Modrinth link to MissingClothConfigScreen (and add translations)

--- common/src/main/java/dzwdz/chat_heads/config/MissingClothConfigScreen.java
+++ common/src/main/java/dzwdz/chat_heads/config/MissingClothConfigScreen.java
@@ -15,12 +15,14 @@ public class MissingClothConfigScreen extends Screen {
 
 	private final Screen parent;
 	private final Component message;
-	private final Component clothConfigLink;
+	private final Component clothConfigCfLink;
+	private final Component clothConfigMrLink;
 
 	public MissingClothConfigScreen(Screen parent) {
 		super(Component.translatable("text.chat_heads.config.error.title"));
 		this.message = Component.translatable("text.chat_heads.config.error.no_cloth_config");
-		this.clothConfigLink = Component.translatable("text.chat_heads.config.error.cloth_config_link");
+		this.clothConfigCfLink = Component.translatable("text.chat_heads.config.error.cloth_config_curseforge_link");
+		this.clothConfigMrLink = Component.translatable("text.chat_heads.config.error.cloth_config_modrinth_link");
 		this.parent = parent;
 	}
 
@@ -33,8 +35,14 @@ public class MissingClothConfigScreen extends Screen {
 		int x = width / 2 - w / 2; // centered
 		int y = 120;
 
-		addRenderableWidget(Button.builder(clothConfigLink, (button) -> {
+		addRenderableWidget(Button.builder(clothConfigCfLink, (button) -> {
 			Util.getPlatform().openUri("https://www.curseforge.com/minecraft/mc-mods/cloth-config");
+		}).bounds(x, y, w, h).build());
+
+		y += h + 10;
+
+		addRenderableWidget(Button.builder(clothConfigMrLink, (button) -> {
+			Util.getPlatform().openUri("https://modrinth.com/mod/cloth-config");
 		}).bounds(x, y, w, h).build());
 
 		y += h + 10;
