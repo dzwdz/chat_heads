0c8f3859885bafa3e95a113864497293328036ce
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2024-05-03 12:50:42 +0200

    add chat heads to quark's item sharing messages

---
+++ forge/src/main/java/dzwdz/chat_heads/forge/MixinPlugin.java
@@ -0,0 +1,61 @@
+package dzwdz.chat_heads.forge;
+
+import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+	public static boolean isModInstalled(String modId) {
+		// ModList seems to always be null when shouldApplyMixin is executed
+		// While not ideal, we can check which mods are loading
+		for (ModInfo mod : FMLLoader.getLoadingModList().getMods())
+			if (mod.getModId().equals(modId))
+				return true;
+
+		return false;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		if (mixinClassName.endsWith("QuarkShareItemS2CMessageMixin")) {
+			return isModInstalled("quark");
+		}
+
+		return true;
+	}
+
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return "";
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return List.of();
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
---
+++ forge/src/main/java/dzwdz/chat_heads/forge/mixin/compat/QuarkShareItemS2CMessageMixin.java
@@ -0,0 +1,33 @@
+package dzwdz.chat_heads.forge.mixin.compat;
+
+import dzwdz.chat_heads.ChatHeads;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+@Pseudo
+@Mixin(targets = "org.violetmoon.quark.base.network.message.ShareItemS2CMessage")
+public abstract class QuarkShareItemS2CMessageMixin {
+	@Shadow(remap = false) UUID senderUuid;
+
+	@Inject(
+		method = "lambda$receive$0()V",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/gui/components/ChatComponent;addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;Lnet/minecraft/client/GuiMessageTag;)V"
+		)
+	)
+	private void chatheads$setOwner(CallbackInfo ci) {
+		ClientPacketListener connection = Minecraft.getInstance().getConnection();
+		if (connection != null) {
+			ChatHeads.lastSender = connection.getPlayerInfo(senderUuid);
+		}
+	}
+}
