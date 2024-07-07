c72d3e41b4550946017ed79f5dafdf9edf9a6d41
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2024-07-07 21:07:54 +0200

    get rid of locals capture

--- common/src/main/java/dzwdz/chat_heads/mixin/SkinManagerMixin.java
+++ common/src/main/java/dzwdz/chat_heads/mixin/SkinManagerMixin.java
@@ -1,30 +1,27 @@
 package dzwdz.chat_heads.mixin;
 
-import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import dzwdz.chat_heads.mixininterface.TextureLocationSettable;
 import net.minecraft.client.renderer.texture.HttpTexture;
 import net.minecraft.client.resources.SkinManager;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.nio.file.Path;
-import java.util.concurrent.CompletableFuture;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(SkinManager.TextureCache.class)
 public abstract class SkinManagerMixin {
-    @Inject(
+    @ModifyArgs(
             method = "registerTexture(Lcom/mojang/authlib/minecraft/MinecraftProfileTexture;)Ljava/util/concurrent/CompletableFuture;",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/renderer/texture/TextureManager;register(Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/client/renderer/texture/AbstractTexture;)V"
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
+            )
     )
-    public void chatheads$rememberTextureLocation(MinecraftProfileTexture minecraftProfileTexture, CallbackInfoReturnable<CompletableFuture<ResourceLocation>> cir, String string, ResourceLocation id, Path path, CompletableFuture<?> completableFuture, HttpTexture httpTexture) {
+    public void chatheads$rememberTextureLocation(Args args) {
+        ResourceLocation id = args.get(0);
+        HttpTexture httpTexture = args.get(1);
+
         if (id.getPath().startsWith("skins/")) {
             ((TextureLocationSettable) httpTexture).chatheads$setTextureLocation(id);
         }
