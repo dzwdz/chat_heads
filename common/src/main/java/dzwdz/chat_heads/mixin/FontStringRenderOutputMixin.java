edae14d13dc278710b8f7abbea2e8e9c3b47fe69
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2024-09-11 15:29:15 +0200

    simplify charsRendered

--- common/src/main/java/dzwdz/chat_heads/mixin/FontStringRenderOutputMixin.java
+++ common/src/main/java/dzwdz/chat_heads/mixin/FontStringRenderOutputMixin.java
@@ -4,15 +4,14 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import dzwdz.chat_heads.ChatHeads;
 import dzwdz.chat_heads.HeadData;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Style;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Font.StringRenderOutput.class)
@@ -22,11 +21,8 @@ public abstract class FontStringRenderOutputMixin {
     @Shadow @Final private Matrix4f pose;
     @Shadow @Final private boolean dropShadow;
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    public void chatheads$resetCharacterCount(Font font, MultiBufferSource bufferSource, float x, float y, int color, boolean dropShadow, Matrix4f pose, Font.DisplayMode mode, int packedLightCoords, CallbackInfo ci) {
-        // once for dropShadow = true, once for dropShadow = false
-        ChatHeads.charsRendered = 0;
-    }
+    @Unique
+    private int chatheads$charsRendered = 0;
 
     @Inject(method = "accept", at = @At("HEAD"))
     public void chatheads$renderChatHead(int i, Style style, int j, CallbackInfoReturnable<Boolean> cir) {
@@ -35,7 +31,7 @@ public abstract class FontStringRenderOutputMixin {
 
         int renderIndex = Math.max(ChatHeads.renderHeadData.codePointIndex(), 0); // fallback to rendering at beginning
 
-        if (ChatHeads.charsRendered == renderIndex) {
+        if (chatheads$charsRendered == renderIndex) {
             if (!dropShadow) {
                 PoseStack poseStack = ChatHeads.guiGraphics.pose();
 
@@ -51,6 +47,6 @@ public abstract class FontStringRenderOutputMixin {
             x += 8 + 2;
         }
 
-        ChatHeads.charsRendered++;
+        chatheads$charsRendered++;
     }
 }
