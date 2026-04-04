fe2210a62f3b15f071b78ac98d88afa6dd039b7a
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2026-04-04 19:47:36 +0200

    fix click position (as in 1.21.6)

--- common/src/main/java/dzwdz/chat_heads/mixin/ChatComponentMixin.java
+++ common/src/main/java/dzwdz/chat_heads/mixin/ChatComponentMixin.java
@@ -1,10 +1,10 @@
 package dzwdz.chat_heads.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import dzwdz.chat_heads.ChatHeads;
 import dzwdz.chat_heads.HeadData;
 import dzwdz.chat_heads.config.RenderPosition;
@@ -14,32 +14,13 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.util.FormattedCharSequence;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ChatComponent.class, priority = 990) // apply before Quark's ChatComponentMixin
 public abstract class ChatComponentMixin {
-    @Unique
-    int chatheads$chatOffset; // used in render and getTagIconLeft
-
-    @ModifyVariable(
-            method = "render",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/GuiMessage$Line;addedTime()I"
-            )
-    )
-    public GuiMessage.Line chatheads$captureGuiMessage(GuiMessage.Line guiMessage,
-            @Share("guiMessage") LocalRef<GuiMessage.Line> guiMessageRef) {
-        guiMessageRef.set(guiMessage);
-        chatheads$chatOffset = ChatHeads.getChatOffset(guiMessage);
-        return guiMessage;
-    }
-
     @ModifyArg(
             method = "render",
             at = @At(
@@ -50,15 +31,15 @@ public abstract class ChatComponentMixin {
             index = 2
     )
     public int chatheads$moveText(Font font, FormattedCharSequence formattedCharSequence, int x, int y, int color,
-            @Share("y") LocalIntRef yRef, @Share("opacity") LocalFloatRef opacityRef) {
+            @Local GuiMessage.Line guiMessage, @Share("y") LocalIntRef yRef, @Share("opacity") LocalFloatRef opacityRef) {
         yRef.set(y);
         opacityRef.set((((color >> 24) + 256) % 256) / 255f); // haha yes
-        return chatheads$chatOffset;
+        return ChatHeads.getChatOffset(guiMessage);
     }
 
     @ModifyExpressionValue(method = "getTagIconLeft", at = @At(value = "CONSTANT", args = "intValue=4"))
-    private int chatheads$moveTagIcon(int four) {
-        return four + chatheads$chatOffset;
+    private int chatheads$moveTagIcon(int four, @Local(argsOnly = true) GuiMessage.Line guiMessage) {
+        return four + ChatHeads.getTextWidthDifference(guiMessage);
     }
 
     @Inject(
@@ -70,8 +51,8 @@ public abstract class ChatComponentMixin {
             )
     )
     public void chatheads$renderChatHead(GuiGraphics guiGraphics, int tickCount, int mouseX, int mouseY, boolean focused, CallbackInfo ci,
-            @Share("guiMessage") LocalRef<GuiMessage.Line> guiMessage, @Share("y") LocalIntRef yRef, @Share("opacity") LocalFloatRef opacityRef) {
-        HeadData headData = ChatHeads.getHeadData(guiMessage.get());
+            @Local GuiMessage.Line guiMessage, @Share("y") LocalIntRef yRef, @Share("opacity") LocalFloatRef opacityRef) {
+        HeadData headData = ChatHeads.getHeadData(guiMessage);
         if (headData == HeadData.EMPTY)
             return;
 
@@ -99,12 +80,6 @@ public abstract class ChatComponentMixin {
         ChatHeads.renderHeadData = HeadData.EMPTY;
     }
 
-    @ModifyVariable(method = "getClickedComponentStyleAt", at = @At("STORE"))
-    public GuiMessage.Line chatheads$updateChatOffset(GuiMessage.Line guiMessage, @Share("chatOffset") LocalIntRef chatOffsetRef) {
-        chatOffsetRef.set(ChatHeads.getChatOffset(guiMessage));
-        return guiMessage;
-    }
-
     @ModifyArg(
             method = "getClickedComponentStyleAt",
             at = @At(
@@ -113,8 +88,8 @@ public abstract class ChatComponentMixin {
             ),
             index = 1
     )
-    public int chatheads$correctClickPosition(int x, @Share("chatOffset") LocalIntRef chatOffsetRef) {
-        return x - chatOffsetRef.get();
+    public int chatheads$correctClickPosition(int x, @Local GuiMessage.Line guiMessage) {
+        return x - ChatHeads.getTextWidthDifference(guiMessage);
     }
 
     @ModifyExpressionValue(
@@ -126,7 +101,7 @@ public abstract class ChatComponentMixin {
     )
     public int chatheads$fixTextOverflow(int original) {
         // at this point, neither guiMessage nor chatOffset are well-defined
-        return original - ChatHeads.getChatOffset(ChatHeads.getLineData());
+        return original - ChatHeads.getTextWidthDifference(ChatHeads.getLineData());
     }
 
     @Inject(
